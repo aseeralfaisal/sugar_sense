f0cb5427880a9b4dbe1b2db3e31a92d51dae7ff7
aseeralfaisal <aseeralfaisal@gmail.com>
2022-04-13 04:59:59 +0600

    hello

--- app/main.py
+++ app/main.py
@@ -1,30 +1,31 @@
-from flask import Flask, request, jsonify
-import numpy as np
-import pickle
+from flask import Flask
+# from flask import Flask, request, jsonify
+# import numpy as np
+# import pickle
 
-model = pickle.load(open('final_model.pkl', 'rb'))
+# model = pickle.load(open('final_model.pkl', 'rb'))
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return "Hello world"
+def home_view():
+    return "<h1>Hi Tasin</h1>"
 
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    Pregnancies = request.form.get('Pregnancies')
-    Glucose = request.form.get('Glucose')
-    BloodPressure = request.form.get('BloodPressure')
-    SkinThickness = request.form.get('SkinThickness')
-    Insulin = request.form.get('Insulin')
-    BMI = request.form.get('BMI')
-    DiabetesPedigreeFunction = request.form.get('DiabetesPedigreeFunction')
-    Age = request.form.get('Age')
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     Pregnancies = request.form.get('Pregnancies')
+#     Glucose = request.form.get('Glucose')
+#     BloodPressure = request.form.get('BloodPressure')
+#     SkinThickness = request.form.get('SkinThickness')
+#     Insulin = request.form.get('Insulin')
+#     BMI = request.form.get('BMI')
+#     DiabetesPedigreeFunction = request.form.get('DiabetesPedigreeFunction')
+#     Age = request.form.get('Age')
 
-    input_query = np.array(
-        [[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
-    result = model.predict(input_query)[0]
+#     input_query = np.array(
+#         [[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+#     result = model.predict(input_query)[0]
 
-    return jsonify({'Diabetes': str(result)})
+#     return jsonify({'Diabetes': str(result)})
